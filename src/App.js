9365402812a1dac9f4f02facd4d8b7475eeab2b0
Armando Nery <wespenry@hotmail.com>
2020-08-25 20:51:12 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
       init(myElement, { 
         showCursor: false,
         loop: true,
-        strings: ['Creative', 'Focused', 'Decided'],
+        strings: ['Creative (•◡•)', 'Focused (~˘▾˘)~', 'Decided ˙ ͜ʟ˙'],
         typeSpeed: 100,
         startDelay: 500,
         backDelay: 500
@@ -16,7 +16,7 @@ import './App.css'
     }
     render(){
       return (
-        <div>
+        <div className="App">
           <h1>Web Developer</h1>
           <p id="myElement"></p>
         </div>
