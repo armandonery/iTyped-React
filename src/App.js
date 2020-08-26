280311b6af0ff3a29032015e3b37a66d72454015
Armando Nery <wespenry@hotmail.com>
2020-08-25 21:00:51 -0500

    last

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css'
       init(myElement, { 
         showCursor: false,
         loop: true,
-        strings: ['Creative (•◡•)', 'Focused (~˘▾˘)~', 'Decided ˙ ͜ʟ˙'],
+        strings: ['Is Creative (•◡•)', 'Keeps Focused (~˘▾˘)~', 'Is Decided ˙ ͜ʟ˙'],
         typeSpeed: 100,
         startDelay: 500,
         backDelay: 500
@@ -17,7 +17,14 @@ import './App.css'
     render(){
       return (
         <div className="App">
-          <h1>Web Developer</h1>
+          <h1>
+            <sup id="sup">
+            I am a
+            </sup>Web Developer
+            <span id="span">
+              who
+            </span>
+          </h1>
           <p id="myElement"></p>
         </div>
       )
